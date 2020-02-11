749f9ef29bf8b6dd112f6112be895edb520749e6
lawlieet <juan.1514@hotmail.com>
2020-02-11 16:50:30 -0600

    dev3

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,28 +1,23 @@
-//exports.isLoggedIn = (req, res, next) => {
-//  req.isAuthenticated() ? next() : res.redirect('/')
-//}
-//
-//exports.isActive = (req, res, next) => {
-//  req.user.active ? next() : res.send('Confirm your account')
-//}
-//
-//exports.nav = (req, res, next) => {
-//  if (req.isAuthenticated()) {
-//    req.app.locals.logged = true
-//  } else {
-//    req.app.locals.logged = false
-//  }
-//  next()
-//}
+exports.isLoggedIn = (req, res, next) => {
+  req.isAuthenticated() ? next() : res.redirect('/')
+}
 
- 
-exports.isAuth = (req, res, next) => {
+exports.isActive = (req, res, next) => {
+  req.user.active ? next() : res.send('Confirm your account')
+}
+
+exports.isActive = (req,res,next) => {
+  req.user.status=="Active" ? next() : res.send('Confirma tu correo')
+} 
+
+exports.nav = (req, res, next) => {
   if (req.isAuthenticated()) {
-    next();
+    req.app.locals.logged = true
   } else {
-    res.redirect("/");
+    req.app.locals.logged = false
   }
-};
+  next()
+}
 
 exports.isAdmin = (req, res, next) => {
   if (req.isAuthenticated() && req.user.role === "ADMIN") {
