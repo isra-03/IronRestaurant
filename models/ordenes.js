e0c59c3fb1319e99a6659fe357b1dc66eae37a20
israSH <isra-03p@hotmail.com>
2020-02-11 12:47:32 -0600

    registro de usuario con correo de ingreso

---
+++ models/ordenes.js
@@ -0,0 +1,13 @@
+const { model, Schema } = require("mongoose");
+const userSchema = new Schema(
+  {   
+    idMesero:String,
+    idPlatillo:String,
+    mesa: Number,
+    cantidad: { type: Number, min: 1, max: 5},
+    total:Number
+  }
+  );
+  
+  
+  module.exports = model("User", userSchema);
